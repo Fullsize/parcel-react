84fbd9dba32865bc0caec9794424212768918fc7
无涯 <yutaozhou@YutaodeMacBook-Pro.local>
2022-08-26 10:36:51 +0800

    feat: 增加相应配置

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 /*
  * @Date: 2022-08-25 10:26:54
  * @LastEditors: Fullsize
- * @LastEditTime: 2022-08-25 18:01:28
+ * @LastEditTime: 2022-08-26 09:49:38
  * @FilePath: /parceljs-react/index.js
  * @Author: Fullsize
  */
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./src/app";
-const app = document.getElementById("app");
 import { HashRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(
   document.getElementById("app")
